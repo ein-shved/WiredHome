61f4ffecfdfbed1bb8331105685a27f248175ad4
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-03-12 12:46:26 +0300

    Add tty control scripts

--- flake.nix
+++ flake.nix
@@ -1,12 +1,63 @@
 {
   description = "The template project for stm32 with logging to uart";
   inputs = {
+    nixpkgs.url = "nixpkgs";
     stm32.url = github:ein-shved/nix-stm32;
+    stm32.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, stm32, ... } :
-  stm32.mkFirmware {
-    mcu = stm32.mcus.stm32f103;
+  outputs = { self, stm32, nixpkgs } :
+  let
     name = "stm32-uart-log";
-    src = ./.;
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    findTty = pkgs.writeShellScriptBin "findTty" ''
+      set -e
+      function getoneof() {
+        files="$(eval echo "$*")"
+        nfiles="$(echo "$files" | wc -w)"
+        if [ x"$nfiles" == x"1" ] && [ -e "$files" ]; then
+          echo "$files"
+          exit 0
+        else
+          echo "Found several files for operation: $*" >&2
+        fi
+        exit 1
+      }
+      tty="$1";
+      if [ -z "$tty" ]; then
+        if ! tty=$(getoneof '/dev/ttyUSB?'); then
+          if ! tty=$(getoneof '/dev/ttyACM?'); then
+            echo "No suitable tty device found. Please specify one" >&2;
+            exit 1;
+          fi
+        fi
+      fi
+      echo "$tty"
+    '';
+    stm32stty = pkgs.writeShellScriptBin "stm32stty" ''
+      set -e
+      tty="$(${findTty}/bin/findTty "$@")"
+      ${pkgs.coreutils}/bin/stty -F "$tty" 115200 -icrnl
+    '';
+    stm32catty = pkgs.writeShellScriptBin "stm32catty" ''
+      set -e
+      tty="$(${findTty}/bin/findTty "$@")"
+      ${stm32stty}/bin/stm32stty "$tty" && exec cat "$tty"
+    '';
+    firmware = stm32.mkFirmware {
+      inherit name;
+      mcu = stm32.mcus.stm32f103;
+      src = ./.;
+    };
+  in firmware // {
+    inherit stm32catty stm32stty;
+    scripts = pkgs.symlinkJoin {
+      name = "${name}-scripts";
+      paths = [
+        firmware.flasher
+        firmware.debugger
+        stm32catty
+        stm32stty
+      ];
+    };
   };
 }
